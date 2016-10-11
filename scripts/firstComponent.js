4d5805bd058e80ab196058f7bdfe2f900fc07c9e
eternity8 <joelmcd42@gmail.com>
2016-10-11 15:48:58 +1030

    basic zooming works. Starting on figures

--- scripts/firstComponent.js
+++ scripts/firstComponent.js
@@ -1,12 +1,14 @@
 
 width = 350;
 height = 350;
-var mySVG = d3.select("svg").attr("width",width)
+var mySVG = d3.select("#zoomBox").attr("width",width)
                 .attr("height",height);
 
 d3.csv("http://localhost/data/slavedata3.csv", function(myData){
+              //Display something to console
               console.log(myData[0]);
 
+              //Functions for defining data point positions
               function makeRadius(year){
                 return (year-1514)/352 * width/2;
               }
@@ -14,25 +16,25 @@ d3.csv("http://localhost/data/slavedata3.csv", function(myData){
               function makeTheta(i){
                 return i;
               }
-
+              //Functions for applying zoom
               function myZoomHandler(){
-                mySVG.select("g").attr("transform","translate("+zoom.event.translate+ ")");
+                mySVG.select("g").attr("transform", "translate(" + width/2 +", " + height/2 +") scale("+d3.event.transform.k+ ")");
               }
-              var zoom = d3.zoom().scaleExtent([1/10,2]).on("zoom",myZoomHandler);
+              var zoom = d3.zoom().scaleExtent([1,50]).on("zoom",myZoomHandler);
 
-              mySVG.select("g").call(zoom);
+              mySVG.call(zoom);
 
               var circles = mySVG.append("g").attr("transform",function(){return "translate(" + width/2 + ", " + height/2 + ")";}).selectAll("circle")
                   .data(myData).enter().append("circle");
 
-              d3.select("svg").append("circle").attr("cx",0)
+              d3.select("svg").select("g").append("circle").attr("cx",0)
                                                .attr("cy",0)
-                                               .attr("r",1)
+                                               .attr("r",0.4)
                                                .attr("id","Middle");
 
               circles.attr("cx",function(d,i){return makeRadius(d.yearam)*Math.cos(makeTheta(i));})
                      .attr("cy",function(d,i){return makeRadius(d.yearam)*Math.sin(makeTheta(i));})
-                     .attr("r",1);
+                     .attr("r",0.4);
 
   //            function addValToObject(myObject){myObject["newKey"]="Hello";};
 
