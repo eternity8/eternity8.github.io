ce158397400eb99dff49af3db980531e4503fa43
eternity8 <joelmcd42@gmail.com>
2016-10-22 14:26:12 +1030

    canvas version working pretty well. Added zoom preferences. Could refine zoom more another time.

--- scripts/firstComponentCanvas3.js
+++ scripts/firstComponentCanvas3.js
@@ -1,13 +1,20 @@
 //Globals
 
+//DOM Elements
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var textFields = d3.selectAll(".paneltext");
+
 //Canvas Dimensions
 var canvasWidth = 600;
 var canvasHeight = 600;
 var viewradius = canvasWidth/2 - 20;
+d3.select(canvas).attr("width",canvasWidth);
+d3.select(canvas).attr("height",canvasHeight);
 
 //Zoom Interactive Defaults
 var circleSize = 0.002*viewradius
-var startScale=1;
+var startScale=2;
 var maxScale=400;
 var minScale=1;
 
@@ -22,10 +29,7 @@ var twoPi = Math.PI*2;
 var minYear = 1514;
 var maxYear = 1866;
 
-//DOM Elements
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var textFields = d3.selectAll(".paneltext");
+
 
 //Legend Dimensions
 var legendRectWidth = 40;
@@ -33,7 +37,7 @@ var legendRectSpacing = 5;
 var legendPerColumn = 4;
 var legendTextWidth = 320;
 
-//EssentialSlaveData variables: yearam, embarked, disembarked, rand, landingRegion, baseRadius
+//EssentialSlaveData variables: yearam, embarked, disembarked, rand, landingRegion, baseRadius, shiftedYear
 d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
   //Regions variables: regionName, percentVisible, percentRadians, minRadians, maxRadians, color
   d3.csv("http://localhost/data/regions.csv", function(dRegions){
@@ -59,18 +63,27 @@ d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
       disembarked: 166,
       total: 1,
       year: 1514,
-      regionCount: [0,0,0,0,0,0,0,0]
+      regionCount: [1,0,0,0,0,0,0,0]
     };
     //General variables that update once per zoom loop
     //May remove last three if redundant
     var current = {
       year: minYear,
+      yearStep: 5,
       scaleFactor: startScale,
       zoomout: false, //1 zoom-out, -1 zoom-in
       newEmbarked: 0,
       newDisembarked: 0,
       newAdded: 0,
-      scrolls:0
+      scrollCount:0
+    };
+    //preferences
+    var preferences = {
+      //adjust zoom scaling method (true: add/minus years, false: times/divide current year by factor)
+      zoomIsLinear: true,
+      zoomSpeed: 1,
+      //toggles whether scroll speed affects zoom speed
+      useDelta: true
     };
 
     //---one-time-functions---
@@ -86,6 +99,7 @@ d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
         currentV.rand = +currentV.rand;
         currentV.landingRegion = +currentV.landingRegion;
         currentV.baseRadius = +currentV.baseRadius; //1514=0,1866=1, scaled
+        currentV.shiftedYear = +currentV.shiftedYear;
         currentV.x=0;
         currentV.y=0;
       }
@@ -118,6 +132,11 @@ d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
         current.zoomout = false;
       }
     }
+    function updateScaleLinear(deltaY){
+      if (preferences.useDelta){
+
+      }
+    }
     function updateScaleFactor(deltaY){
       //do Nothing if scaleFactor out of bounds
       if(current.zoomout){
@@ -126,16 +145,42 @@ d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
       current.scaleFactor*=(10/11);
       }
     }
+    //Added preferences, haven't added zoomSpeed in. Current useDelta adjustment is really bad.
+    function updateYear(deltaY){
+      //Case: linear Zooming (year spacing consistent)
+      if(preferences.zoomIsLinear){
+        var yearStep = preferences.zoomSpeed;
 
-    function updateYear(){
-      if (current.zoomout){
-        current.year++;
-        current.scrolls++;
-      } else {
-        current.year--;
-        current.scrolls--;
+        if (preferences.useDelta){
+          yearStep += (
+            Math.abs(deltaY/50));
+        }
+        if (!current.zoomout){
+          yearStep *= (-1)
+        }
+        current.year+=yearStep;
       }
-      console.log(current.scrolls);
+      else {
+        //Case: zooming by ratio (50%, 100% zoom etc.)
+        var yearFactor = current.scaleFactor;
+        if (!current.zoomout){
+          yearFactor = -1/yearFactor;
+        }
+        if (preferences.useDelta){
+          //put something here to make delta affect yearFactor
+        }
+        console.log(current.scrollCount);
+        current.year+=yearFactor*(current.scrollCount);
+      }
+      current.year = Math.floor(current.year);
+      if (current.year<minYear)
+      {
+        current.year=minYear;
+      }
+      if (current.year>maxYear)
+      {
+        current.year=maxYear;
+      }
     }
     //Saves current visible data in lastVisible
     function storeLastVisible(){
@@ -146,41 +191,49 @@ d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
       lastVisible.disembarked = visible.disembarked;
     }
     function updateVisible(){
-      //Note: assumes that year changes by at least one with each zoom update
       var currentIndex = visible.index;
-      var scaledYear = current.year;
+      var updatedYear = current.year;
       var voyage;
       current.newAdded = 0;
       current.newEmbarked = 0;
       current.newDisembarked = 0;
+      //zoom out, i starts at one above last visible index, ends at highest entry for current year
       if (current.zoomout){
-        while(currentIndex<totalVoyages-1){
-          currentIndex++;
-          voyage = Voyages[currentIndex];
-          if(voyage.yearam>scaledYear){
+        for(i=currentIndex+1;i<totalVoyages;i++){
+          //break condition
+          if(Voyages[i].yearam>updatedYear){
+            i--;
+            //console.log("Breaking at index " + i);
             break;
           }
+          //console.log("adding... i = " + i);
+          voyage = Voyages[i];
           current.newEmbarked += voyage.embarked;
           current.newDisembarked += voyage.disembarked;
           current.newAdded++;
           visible.regionCount[voyage.landingRegion]++;
-
         }
-        currentIndex--;
+        if (i>=totalVoyages){
+          i--;
+        }
+        visible.index=i;
       } else {
-        while((currentIndex>0) && (Voyages[currentIndex].yearam>=scaledYear) ){
-          voyage = Voyages[currentIndex];
+        //zoom in, i starts at last visible index, ends at highest entry for current year
+        for(i=currentIndex;i>0;i--){
+          if(Voyages[i].yearam<=updatedYear){
+            break;
+          }
+          //console.log("removing... i = " + i);
+          voyage = Voyages[i];
           current.newEmbarked -= voyage.embarked;
           current.newDisembarked -= voyage.disembarked;
           current.newAdded--;
           visible.regionCount[voyage.landingRegion]--;
-          currentIndex--;
         }
-        currentIndex++;
+          visible.index=i;
       }
 
       //Note: current values could be negative
-      visible.index = currentIndex;
       visible.total+=current.newAdded;
       visible.embarked+=current.newEmbarked;
       visible.disembarked+=current.newDisembarked;
@@ -197,7 +250,7 @@ d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
 
       var thetaRange;
       var tempMinTheta=0;
-  //    console.log("start "+ visible.regionCount);
+//      console.log("start "+ visible.regionCount);
       for(i = 0;i<8;i++)
       {
   //      console.log("Before " + dRegions[i].minRadians,dRegions[i].percentRadians);
@@ -210,19 +263,27 @@ d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
     }
     function updateCirclePositions(){
         var total = visible.index;
+        var scaleYear = current.year-1513;
+        var scale = viewradius/scaleYear;
+        circleSize=scale/2;
+      //  console.log(circleSize);
         var x, y, theta, r, color, regionID;
-        for (i=0;i<total;i++){
+        ctx.clearRect(0,0,canvasWidth,canvasHeight);
+        ctx.save();
+        ctx.translate(canvasWidth/2,canvasHeight/2);
+        for (i=0;i<=total;i++){
           regionID = Voyages[i].landingRegion;
-          r = Voyages[i].baseRadius*current.scaleFactor;
-          theta = Voyages[i].rand * dRegions[regionID].percentRadians + dRegions[regionID].minTheta;
+          r = Voyages[i].shiftedYear*scale;
+          theta = Voyages[i].rand * dRegions[regionID].percentRadians + dRegions[regionID].minRadians;
           x = r*Math.cos(theta);
           y = r*Math.sin(theta);
-    //      console.log(x+ " " + y + " " + r + " " + theta + " " + regionID + dRegions[regionID].minTheta);
+    //    console.log(x+ " " + y);
           ctx.beginPath()
           ctx.fillStyle = dRegions[regionID].color;
-          ctx.arc(x,y,10,0,twoPi,1);
+          ctx.arc(x,y,circleSize,0,twoPi,1);
           ctx.fill();
         }
+        ctx.restore();
     }
     function drawCircles(){
 
@@ -236,17 +297,23 @@ d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
 
     //ZOOM HANDLER FUNCTION
     function myZoomHandler(event){
+      event.preventDefault();
+//      console.log(event.deltaY);
       updateDirection(event.deltaY);
 
       //Do nothing if already at full zoom level
-      if( ((current.zoomout) && (current.year == maxYear)) ||
+      if( ((current.zoomout) && (current.year== maxYear)) ||
           ((!current.zoomout) && (current.year == minYear)) )
       {
         return;
       }
-
-      updateScaleFactor(event.deltaY);
-      updateYear();
+      if(current.zoomout){
+        current.scrollCount++;
+      }
+      else{
+        current.scrollCount--;
+      }
+      updateYear(event.deltaY);
       storeLastVisible();
       updateVisible();
       updateTextPanel();
@@ -256,9 +323,26 @@ d3.csv("http://localhost/data/essentialSlaveData.csv", function(Voyages){
 
     }
 
+    //KEYBOARD HANDLER
+    function keyHandler(event){
+      key = event.key;
+      if (key=="a")
+      {
+        if(preferences.zoomSpeed>1){
+          preferences.zoomSpeed--;
+        }
 
+      }
+      else if (key =="d")
+      {
+        preferences.zoomSpeed++;
+      }
+      console.log(preferences.zoomSpeed);
+    }
 
+    updateCirclePositions();
     //Add Zoom Event Listener
     myCanvas.addEventListener("wheel",myZoomHandler);
+    window.addEventListener("keydown",keyHandler)
   });
 });
